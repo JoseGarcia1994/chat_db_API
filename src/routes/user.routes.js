3b5db0538d4169d29bd88bfb956b387abef26346
Jose Garcia <garciacolin@hotmail.com>
2023-08-03 17:57:17 -0600

    Chat API V1.3

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 const {Router} = require('express');
 const {createUser, loginUser, getAllUsers} = require('../controllers/users.controllers');
 const {loginUserValidator, registerUserValidator} = require('../validators/users.validators');
-const authenticate = require('../middlewares/auth.middleware');
+const authenticate = require("../middlewares/auth.middleware");
+
 
 const router = Router();
 
@@ -11,5 +12,9 @@ router.post("/login", loginUserValidator, loginUser);
 
 router.get('/users', getAllUsers);
 
+router.get("/users", authenticate, (req, res) => {
+     console.log(req);
+     res.send("users");
+   });
 
 module.exports = router;
